7f2a27caa167b8213e2f1eecd193604cece52076
Keith <kiely-xbox@live.com>
2016-08-09 15:28:34 -0500

    App done. Version 1

--- app/src/main/java/com/example/xdog/dartsscore/GamerOverDialog.java
+++ app/src/main/java/com/example/xdog/dartsscore/GamerOverDialog.java
@@ -8,18 +8,13 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-/**
- * Created by Jimena on 20/07/2016.
- */
 public class GamerOverDialog extends DialogFragment {
-    private Button doneB;
-    private TextView rWins, rLose, rounds, name;
     private View rootView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.game_over_dialog, container, false);
 
-        doneB = (Button) rootView.findViewById(R.id.doneButton);
+        Button doneB = (Button) rootView.findViewById(R.id.doneButton);
         doneB.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -34,15 +29,23 @@ public class GamerOverDialog extends DialogFragment {
 
     public void onStart(){
         super.onStart();
-        rWins = (TextView) rootView.findViewById(R.id.numWinTV);
-        rLose = (TextView) rootView.findViewById(R.id.numLoseTV);
-        rounds = (TextView) rootView.findViewById(R.id.numRoundsTV);
-        name = (TextView) rootView.findViewById(R.id.playerNameHeadingTV);
-
-        String roundWins = ""+ Scoreboard.bundle.getInt(Scoreboard.P1_ROUND_WINS);
-        String roundLose = ""+ Scoreboard.bundle.getInt(Scoreboard.P2_ROUND_WINS);
+        TextView rWins = (TextView) rootView.findViewById(R.id.numWinTV);
+        TextView rLose = (TextView) rootView.findViewById(R.id.numLoseTV);
+        TextView rounds = (TextView) rootView.findViewById(R.id.numRoundsTV);
+        TextView name = (TextView) rootView.findViewById(R.id.playerNameHeadingTV);
+        int p1RoundWins = Scoreboard.bundle.getInt(Scoreboard.P1_ROUND_WINS);
+        int p2RoundWins = Scoreboard.bundle.getInt(Scoreboard.P2_ROUND_WINS);
+        String roundWins = ""+ p1RoundWins;
+        String roundLose = ""+ p2RoundWins;
         String numLegs = ""+ Scoreboard.bundle.getInt(Scoreboard.NUM_ROUNDS);
-        String winnersName = "" + getArguments().getString(Scoreboard.PLAYER_NAME);
+        String winnersName;
+        if (p1RoundWins != p2RoundWins)
+        winnersName = "" + getArguments().getString(Scoreboard.PLAYER_NAME);
+        else {
+            winnersName = getResources().getString(R.string.draw);
+            TextView wins = (TextView) rootView.findViewById(R.id.winTV);
+            wins.setVisibility(View.INVISIBLE);
+        }
 
         rWins.setText(roundWins);
         rLose.setText(roundLose);
